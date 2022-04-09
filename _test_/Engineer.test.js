ad03208b49a3b30eddc7ffd9ceb6ec9dc32aa5d2
Chlovanna <ashdmt39@gmail.com>
2022-04-08 23:55:45 -0400

    Created Engineer.test.js

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -1,1 +1,43 @@
+const Employee = require("../lib/Engineer");
 
+describe("Employee", () => {
+  describe("Initialization", () => {
+    it("should return employee name, id, email, and office number ", () => {
+      const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
+
+      expect(worker.name).toEqual("Zoey");
+      expect(worker.id).toEqual("0911");
+      expect(worker.email).toEqual("ashdmt39@gmail.com");
+      expect(worker.Github).toEqual("Chlovanna");
+    });
+  });
+
+  describe("employeeInfo", () => {
+    it("should return employee name", () => {
+      const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "Github");
+      const name = worker.getName();
+      expect(name).toEqual("Zoey")
+    });
+
+    it("should return employee ID", () => {
+        const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "Github");
+        const id = worker.getId();
+        expect(id).toEqual("0911")
+      });
+      it("should return employee email", () => {
+        const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "Github");
+        const email = worker.getEmail();
+        expect(email).toEqual("ashdmt39@gmail.com")
+      });
+      it("should return employee role", () => {
+        const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "Github");
+        const role = worker.getRole();
+        expect(role).toEqual("Employee")
+      });
+      it("should return Github", () => {
+        const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "Github");
+        const Github = worker.getGithub();
+        expect(Github).toEqual("Github")
+      });
+    });
+  });
--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -16,28 +16,28 @@ describe("Employee", () => {
     it("should return employee name", () => {
       const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
       const name = worker.getName();
-      expect(name).toEqual("Zoey")
+      expect(name).toEqual("Zoey");
     });
 
     it("should return employee ID", () => {
-        const worker = new new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
-        const id = worker.getId();
-        expect(id).toEqual("0911")
-      });
-      it("should return employee email", () => {
-        const worker = new new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
-        const email = worker.getEmail();
-        expect(email).toEqual("ashdmt39@gmail.com")
-      });
-      it("should return employee role", () => {
-        const worker = new new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
-        const role = worker.getRole();
-        expect(role).toEqual("Employee")
-      });
-      it("should return office number", () => {
-        const worker = new new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
-        const officeNumber = worker.getOfficeNumber();
-        expect(officeNumber).toEqual("11B")
-      });
+      const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
+      const id = worker.getId();
+      expect(id).toEqual("0911");
+    });
+    it("should return employee email", () => {
+      const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
+      const email = worker.getEmail();
+      expect(email).toEqual("ashdmt39@gmail.com");
+    });
+    it("should return employee role", () => {
+      const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
+      const role = worker.getRole();
+      expect(role).toEqual("Employee");
+    });
+    it("should return office number", () => {
+      const worker = new Employee("Zoey", "0911", "ashdmt39@gmail.com", "11B");
+      const officeNumber = worker.getOfficeNumber();
+      expect(officeNumber).toEqual("11B");
     });
   });
+});
