341e1791403ce1ec95e29a39533bef575bf4ff7f
Chlovanna <ashdmt39@gmail.com>
2022-04-13 23:03:34 -0400

    completed walkthrough video

--- Main/src/Template.js
+++ Main/src/Template.js
@@ -101,7 +101,7 @@ module.exports = (profile) => {
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
           <script src="https://kit.fontawesome.com/1e0a13a89f.js" crossorigin="anonymous"></script>
-          <link rel="stylesheet" href="../../Assets/style.css">
+          <link rel="stylesheet" href="../dist/style.css">
   </head>
   
   <body>
